989ee7cd15a1ae76ca7b6c13b4079dbb5fa61c28
Cheng Qiu <qc2003020403@163.com>
2024-06-27 18:08:14 +0800

    11

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
+import ctypes
 import sys
 import os
+import urllib
+from collections import deque
 from time import sleep
 from typing import List
+import logging
+
+import portalocker
+from PyQt5 import QtWidgets
+from plyer import notification
+import multiprocessing  # For multiprocessing.freeze_support()
 
 import requests
 import json
 import webbrowser
 import threading
 import socket
+
+import win32print
 from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction, QMessageBox, QMainWindow, QLabel, QLineEdit, \
-    QPushButton, QVBoxLayout, QWidget, QListWidget, QListWidgetItem, QHBoxLayout, QTableWidget, QTableWidgetItem
+    QPushButton, QVBoxLayout, QWidget, QListWidget, QListWidgetItem, QHBoxLayout, QTableWidget, QTableWidgetItem, \
+    QDialog
 from PyQt5.QtGui import QIcon, QPixmap, QImage
-from PyQt5.QtCore import Qt, pyqtSignal, QObject, QUrl, QThread
+from PyQt5.QtCore import Qt, pyqtSignal, QObject, QUrl, QThread, QTimer
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 
 import shelve
 
-from print_device import PrintDevice, DeviceSelectionWidget, PrintDeviceStatus, PrintJob
+from print_device import PrintDevice, DeviceSelectionWidget, PrintDeviceStatus, PrintJob, PrintDialog, PleaseLoginDialog
 
 
+# 设置日志记录
+logging.basicConfig(filename='app.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 class UserInfo(object):
     username:str
     name:str
@@ -32,9 +47,10 @@ class UserInfo(object):
 class PrintJobSignal(QObject):
     new_print_job = pyqtSignal(list)
     device_update = pyqtSignal(list)
-    device_status_update = pyqtSignal(PrintDeviceStatus)
+    device_status_update = pyqtSignal(object)
+    reviced_print_file = pyqtSignal(str)
 class LoginSignal(QObject):
-    login_error = pyqtSignal()
+    login_error = pyqtSignal(object)
     update_info = pyqtSignal(UserInfo)
 
 
@@ -68,8 +84,8 @@ class CurrentPrintJobWindow(QWidget):
     def refresh_table(self,print_jobs:List[PrintJob]):
         self.jobs_table_widget.setRowCount(len(print_jobs))
         for row, job in enumerate(print_jobs):
-            print(row)
-            print(job)
+            logging.info(row)
+            logging.info(job)
             self.jobs_table_widget.setItem(row, 5, QTableWidgetItem(job.id))
             self.jobs_table_widget.setItem(row, 1, QTableWidgetItem(job.document_name))
             self.jobs_table_widget.setItem(row, 2, QTableWidgetItem(job.job_status))
@@ -87,7 +103,7 @@ class CurrentPrintJobWindow(QWidget):
             self.jobs_table_widget.setCellWidget(row, 0, btn_widget)
 
     def cancel_job(self, job_id):
-        print(f"取消打印作业: {job_id}")
+        logging.info(f"取消打印作业: {job_id}")
         # 在这里添加取消打印作业的逻辑
 
 
@@ -103,6 +119,10 @@ class MainWindow(QMainWindow):
         self.token = None
         self.device_selected = None
         self.device_selected_status = None
+        self.Pdialog = None
+        self.print_job_list = deque()
+        self.print_dialog_open = False
+
         self.print_device_list = []
         db = shelve.open('mydata.db', writeback=False)
         if 'token' in db and db['token']!='':
@@ -115,6 +135,7 @@ class MainWindow(QMainWindow):
         self.print_job_signal.new_print_job.connect(self.show_print_job_window)
         self.print_job_signal.device_update.connect(self.device_update)
         self.print_job_signal.device_status_update.connect(self.device_status_update)
+        self.print_job_signal.reviced_print_file.connect(self.handle_file_print)
         self.login_signal.update_info.connect(self.update_user_info)
         self.login_signal.login_error.connect(self.show_login_error)
         self.init_ui()
@@ -129,7 +150,95 @@ class MainWindow(QMainWindow):
         self.check_device_status_thread = threading.Thread(target=self.check_select_device)
         self.check_device_status_thread.daemon = True
         self.check_device_status_thread.start()
+        # 虚拟打印机相关
+        self.start_file_upload_server()
 
+    def process_queue(self):
+
+        if not self.print_dialog_open and self.print_job_list:
+            if self.isHidden():
+                self.show()
+            if not self.logged_in or self.token == None:
+                please_login_dialog = PleaseLoginDialog(self)
+                if please_login_dialog.exec() == QDialog.Accepted:
+                    # 去登陆
+                    if not self.logged_in:
+                        self.start_server_thread()
+                        self.open_login_webpage()
+                return
+            file_to_print = self.print_job_list.popleft()
+            self.show_dialog(file_to_print)
+        else:
+            pass
+            # If no dialog is open and queue is empty, do further processing (e.g., write to file)
+
+    def add_to_queue(self, file_path):
+        self.print_job_list.append(file_path)
+        self.process_queue()
+
+
+    def show_dialog(self, file_path):
+        self.print_dialog_open = True
+        self.Pdialog = PrintDialog(file_path,self)
+        if self.device_selected:
+            self.Pdialog.set_current_device_show(self.device_selected)
+
+        self.Pdialog.finished.connect(self.dialog_closed)
+        if self.Pdialog.exec() == QDialog.Accepted:
+            # 确认打印
+            self.file_path = file_path
+            # 异步请求
+            self.send_print_request(self.Pdialog.startInput.text(),self.Pdialog.endInput.text(),self.Pdialog.directio,self.Pdialog.copiInput.text(),self.Pdialog.mode,self.Pdialog.max_pages)
+            # dialog.deviceId 更新回来 添加确认的文件路径以及打印配置
+            self.Pdialog = None
+            pass
+
+    def send_print_request(self,start_num,end_num,landscape,copies,duplex,total):
+        if not hasattr(self, 'file_path'):
+            logging.info("No file selected")
+            return
+
+        url = "http://easyoa.fun:8081/api/printer/uploadPrintFileForWindows"
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        if self.device_selected==None:
+            self.getMessageSystem("EasyOA打印", "提交打印失败!打印机选择异常")
+            return
+
+        data = {
+            'copies': int(copies),
+            'duplex': duplex,
+            'start_num': int(start_num),
+            'end_num': int(end_num),
+            'landscape': landscape,
+            'total': int(total),
+            'device_id': self.device_selected.id
+        }
+
+        files = {
+            'file': open(self.file_path, 'rb')
+        }
+
+        response = requests.post(url, headers=headers, data=data, files=files)
+        if response.ok:
+            self.getMessageSystem("EasyOA打印", "提交打印成功!")
+
+        else:
+            self.getMessageSystem("EasyOA打印", "提交打印失败!")
+            logging.info("Failed to send logging.info request", response.status_code, response.text)
+    def getMessageSystem(self,title,message):
+        try:
+            notification.notify(
+                title=title,
+                message=message,
+                timeout=3  # seconds
+            )
+        except Exception as e:
+            logging.info(e)
+    def dialog_closed(self):
+        self.print_dialog_open = False
+        self.process_queue()
     def update_user_info(self,user_info:UserInfo):
         self.logged_in = True
         self.name_label.setText(user_info.name)
@@ -138,39 +247,46 @@ class MainWindow(QMainWindow):
         img = QImage.fromData(res.content)
         self.avatar_label.setPixmap(QPixmap.fromImage(img).scaled(40, 40))
         self.statusBar().showMessage('Logged in')
-
+        # 每次登录成功顺便检查下有没有任务在打印队列
+        self.process_queue()
         pass
     def device_update(self,devices:list):
         self.print_device_list = devices
+        if self.Pdialog:
+            self.Pdialog.set_devices(self.print_device_list)
         self.deviceSlection.set_devices(self.print_device_list)
     def device_status_update(self,device_status:PrintDeviceStatus):
         # 设备状态更新
         if device_status != None:
-            print("更新111")
+            logging.info("更新111")
             self.device_selected_status = device_status
             self.deviceSlection.set_device_status(device_status.statusTypeMessage,device_status.listNums)
-            print(device_status.printJobs)
+            if self.Pdialog:
+                self.Pdialog.set_device_status(device_status.statusTypeMessage,device_status.listNums)
+            logging.info(device_status.printJobs)
             self.current_print_job_window.refresh_table(device_status.printJobs)
             return
         # 更新设备异常
         self.current_print_job_window.refresh_table([])
         self.deviceSlection.set_device_status("设备连接异常,请检查!",-1)
-    def show_login_error(self):
+    def show_login_error(self,obj):
         self.logged_in = False
         self.name_label.setText("点击头像登录")
         self.username_label.setText("点击头像登录")
-        self.avatar_label.setPixmap(QPixmap('icon.png').scaled(40, 40))
+        self.avatar_label.setPixmap(QPixmap('nologgin.png').scaled(40, 40))
         pass
     def check_token(self):
         while True:
             if self.token == None:
-                sleep(5)
+                sleep(3)
             else:
                 self.handle_login_token(self.token)
-                sleep(30)
+                sleep(15)
     def update_ui_with_selected_device(self,device):
         # 提供给选择器修改设备
         self.device_selected = device
+        if self.Pdialog:
+            self.Pdialog.set_current_device_show(device)
         db = shelve.open('mydata.db', writeback=False)
         db['device_select'] = device
         db.close()
@@ -193,7 +309,7 @@ class MainWindow(QMainWindow):
             response = requests.get(url, headers=headers)
 
             # 打印响应的文本内容
-            print(response.text)
+            logging.info(response.text)
 
             # 检查响应状态码
             if response.status_code == 200:
@@ -231,7 +347,7 @@ class MainWindow(QMainWindow):
             response = requests.get(url, headers=headers)
 
             # 打印响应的文本内容
-            print(response.text)
+            logging.info(response.text)
 
             # 检查响应状态码
             if response.status_code == 200:
@@ -252,21 +368,24 @@ class MainWindow(QMainWindow):
         pass
     def init_ui(self):
         self.setWindowTitle('Main Window')
-        self.setGeometry(100, 100, 600, 400)
-
+        self.setGeometry(400, 200, 600, 400)
+        self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
+        self.setWindowIcon(QIcon('icon.png'))
         self.tray_icon = QSystemTrayIcon(self)
         self.tray_icon.setIcon(QIcon('icon.png'))
+        self.tray_icon.activated[QtWidgets.QSystemTrayIcon.ActivationReason].connect(self.openMainWindow)
 
         tray_menu = QMenu()
-        open_action = QAction("Open", self)
+        open_action = QAction("打开EasyOA打印管理", self)
         open_action.triggered.connect(self.show)
         tray_menu.addAction(open_action)
 
-        exit_action = QAction("Exit", self)
+        exit_action = QAction("退出", self)
         exit_action.triggered.connect(QApplication.instance().quit)
         tray_menu.addAction(exit_action)
 
         self.tray_icon.setContextMenu(tray_menu)
+
         self.tray_icon.show()
 
         self.statusBar().showMessage('Ready')
@@ -274,9 +393,9 @@ class MainWindow(QMainWindow):
         # Top bar for user information
         self.top_bar = QHBoxLayout()
         self.avatar_label = QLabel(self)
-        self.avatar_label.setPixmap(QPixmap('icon.png').scaled(40, 40))
+        self.avatar_label.setPixmap(QPixmap('nologgin.png').scaled(40, 40))
         self.avatar_label.mousePressEvent = self.avatar_clicked
-        self.name_label = QLabel('Name: Not Logged In')
+        self.name_label = QLabel('Name: 点击头像登录')
         self.username_label = QLabel('Username: -')
 
         self.top_bar.addWidget(self.avatar_label)
@@ -299,6 +418,9 @@ class MainWindow(QMainWindow):
         if not self.logged_in:
             self.start_server_thread()
             self.open_login_webpage()
+    def openMainWindow(self,reason):
+        if reason == QtWidgets.QSystemTrayIcon.DoubleClick:
+            self.show()
 
     def open_login_webpage(self):
         webbrowser.open('http://easyoa.fun:8081/#/client_oauth')
@@ -329,7 +451,13 @@ class MainWindow(QMainWindow):
         event.ignore()
         self.hide()
 
+    def start_file_upload_server(self):
+
+        self.server_thread_upload = threading.Thread(target=self.server_thread_upload_file_func)
+        self.server_thread_upload.daemon = True
+        self.server_thread_upload.start()
     def start_server_thread(self):
+
         self.server_thread = threading.Thread(target=self.server_thread_func)
         self.server_thread.daemon = True
         self.server_thread.start()
@@ -341,7 +469,7 @@ class MainWindow(QMainWindow):
                 params = parse_qs(query)
                 if 'token' in params:
                     token = params['token'][0]
-                    self.send_response(200)
+                    self.send_response_only(200)
                     self.send_header('Content-type', 'text/html')
                     self.end_headers()
                     self.wfile.write(b'Login successful, you can close this window.')
@@ -354,17 +482,52 @@ class MainWindow(QMainWindow):
 
             def handle_code_received(self, token):
                 self.main_window.handle_login_token(token)
+        logging.info("启动")
+        multiprocessing.freeze_support()
 
         httpd = CustomHTTPServer(('localhost', 65431), RequestHandler, self)
         httpd.serve_forever()
+        logging.info("启动1")
 
 
 
 
+    def server_thread_upload_file_func(self):
+        class RequestHandler(BaseHTTPRequestHandler):
+            def do_GET(self):
+                query = urlparse(self.path).query
+                params = parse_qs(query)
+                if 'paths' in params:
+                    paths = params['paths'][0]
+                    self.send_response_only(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    self.wfile.write(b'successful')
+                    decoded_path = urllib.parse.unquote(paths)
+
+                    self.server.handle_code_received(decoded_path)
+
+        class CustomHTTPServer(HTTPServer):
+            def __init__(self, server_address, RequestHandlerClass, main_window):
+                super().__init__(server_address, RequestHandlerClass)
+                self.main_window = main_window
+
+            def handle_code_received(self, path):
+                self.main_window.print_job_signal.reviced_print_file.emit(path)
+
+        multiprocessing.freeze_support()
+
+        httpd = CustomHTTPServer(('127.0.0.1', 9489), RequestHandler, self)
+        httpd.serve_forever()
+
+    def handle_file_print(self,path):
+        # 能拿到pdf文件路径 直接执行打印，此处需要有队列，一个个处理弹窗，如果当前正在弹窗就等待直接添加到list里，每次弹窗结束前会查看list是否存在任务，如果有任务就继续执行最先的任务
+        logging.info(path)
+        self.add_to_queue(path)
 
     def handle_login_token(self, token):
         # Save the token and update the UI
-        print(f"token:{token}")
+        logging.info(f"token:{token}")
         # 此token为java后端自用token不是oauth2的access_token,此token能直接拿到用户id
         # 接口的 URL
         url = 'http://easyoa.fun:8081/api/user/info'
@@ -386,7 +549,7 @@ class MainWindow(QMainWindow):
         if response.status_code == 200:
             # 请求成功，打印响应内容
             resp = response.json()
-            print('用户信息：', resp)
+            logging.info('用户信息：', resp)
             if resp["code"] == 1:
                 userinfo = UserInfo(
                     username=resp["data"]["username"],
@@ -394,13 +557,12 @@ class MainWindow(QMainWindow):
                     avatar=resp["data"]["avatar"]
                 )
                 self.login_signal.update_info.emit(userinfo)
-                print("登录成功")
+                logging.info("登录成功")
                 return
             if resp["code"] == 900:
                 # token过期了
                 self.token = None
-        self.login_signal.login_error.emit()
-
+        self.login_signal.login_error.emit(None)
 
 
 
@@ -410,11 +572,16 @@ def main():
     main_window = MainWindow()
     # main_window.check_login()
 
-    # Simulate receiving a print job with file list
-    # main_window.handle_print_job(['file1.pdf', 'file2.pdf'])
 
     sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
+    lock_file = "app.lock"
+    try:
+        lock = open(lock_file, 'w')
+        portalocker.lock(lock, portalocker.LOCK_EX | portalocker.LOCK_NB)
+    except portalocker.LockException:
+        logging.info("Another instance is already running")
+        sys.exit(0)
     main()
---
+++ readme.md
@@ -0,0 +1,4 @@
+# 打包完别忘复制图标等资源文件，否则无法启动
+
+# http服务器在pyqt5里打包引起的bug 无法启动
+https://github.com/pyinstaller/pyinstaller/issues/8210
